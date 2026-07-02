14e1c856e18f244141e1298748238f9e9d3eefda
Taehyun Noh <taehyunnoh@google.com>
2026-07-02 17:19:44 +0000

    add segue, mov instead of lea, tls rewriting, etc

--- test/lfi/test_lfi_segue.asm
+++ test/lfi/test_lfi_segue.asm
@@ -1,33 +1,34 @@
-;; LFI Macro Package Test Case
+;; LFI Macro Package Test Case (Segue Mode)
 ;;
 ;; How to run:
-;;   $ ./nasm -f elf64 test/lfi/test_lfi.asm -o test/lfi/test_lfi.o
-;;   $ objdump -d test/lfi/test_lfi.o
+;;   $ ./nasm -f elf64 test/lfi/test_lfi_segue.asm -o test/lfi/test_lfi_segue.o
+;;   $ objdump -d test/lfi/test_lfi_segue.o
 ;;
 
+%define LFI_USE_SEGUE 1
 %use lfi
 
 section .text
-    ;; --- Memory Access (No-Segue Mode) ---
-    mov [rax], rbx          ; Rewrite: lea r11d, [rax]; mov [r14 + r11*1], rbx
-    mov rcx, [rdx]          ; Rewrite: lea r11d, [rdx]; mov rcx, [r14 + r11*1]
+    ;; --- Memory Access (Segue Mode) ---
+    mov [rax], rbx          ; Rewrite: mov [gs:eax], rbx
+    mov rcx, [rdx]          ; Rewrite: mov rcx, [gs:edx]
     mov [rsp + 8], rax      ; Safe: rsp relative -> no rewrite
     mov rbx, [rbp - 4]      ; Safe: rbp relative -> no rewrite
     mov rdx, [rel label]    ; Safe: rip relative -> no rewrite
-    mov [fs:rax], rcx       ; Rewrite: mov r11, [r15+32]; lea r11d, [r11+rax]; mov [r14+r11*1], rcx
-    mov rcx, [fs:rdx]       ; Rewrite: mov r11, [r15+32]; lea ecx, [r11+rdx]; mov rcx, [r14+rcx*1]
+    mov [fs:rax], rcx       ; Rewrite: mov r11, [r15+32]; mov [gs:r11d+eax], rcx
+    mov rcx, [fs:rdx]       ; Rewrite: mov r11, [r15+32]; mov rcx, [gs:r11d+edx]
     mov r8, [fs:0]          ; Rewrite: mov r8, [r15+32] (safe TLS read)
     mov [fs:0], r9          ; Rewrite: mov [r15+32], r9 (safe TLS write)
-    xorpd xmm0, [rax]       ; Rewrite: lea r11d, [rax]; xorpd xmm0, [r14 + r11*1]
-    movaps [rdx], xmm1      ; Rewrite: lea r11d, [rdx]; movaps [r14 + r11*1], xmm1
-    prefetcht0 [rax]        ; Rewrite: lea r11d, [rax]; prefetcht0 [r14 + r11*1]
+    xorpd xmm0, [rax]       ; Rewrite: xorpd xmm0, [gs:eax]
+    movaps [rdx], xmm1      ; Rewrite: movaps [gs:edx], xmm1
+    prefetcht0 [rax]        ; Rewrite: prefetcht0 [gs:eax]
 
     ;; Size prefix preservation
-    mov dword [rax], ebx    ; Rewrite: lea r11d, [rax]; mov dword [r14 + r11*1], ebx
-    mov qword [rax], rbx    ; Rewrite: lea r11d, [rax]; mov qword [r14 + r11*1], rbx
-    inc dword [rax]         ; Rewrite: lea r11d, [rax]; inc dword [r14 + r11*1]
+    mov dword [rax], ebx    ; Rewrite: mov dword [gs:eax], ebx
+    mov qword [rax], rbx    ; Rewrite: mov qword [gs:eax], rbx
+    inc dword [rax]         ; Rewrite: inc dword [gs:eax]
 
-    ;; --- Stack Sandboxing ---
+    ;; --- Stack Sandboxing (Same as No-Segue) ---
     add rsp, 8              ; Rewrite: add esp, 8; lea rsp, [rsp+r14] (via db)
     sub rsp, 16             ; Rewrite: sub esp, 16; lea rsp, [rsp+r14] (via db)
     pop rsp                 ; Rewrite: pop r11; mov esp, r11d; lea rsp, [rsp+r14]
@@ -36,8 +37,9 @@ section .text
     ;; --- Control Flow Sandboxing ---
     jmp rax                 ; Rewrite: and eax, -32; add rax, r14; jmp rax
     call rbx                ; Rewrite: and ebx, -32; add rbx, r14; call rbx
-    jmp [rax]               ; Rewrite: lea r11d, [rax]; mov r11, [r14+r11]; ...
-    call [rbx]              ; Rewrite: lea r11d, [rbx]; mov r11, [r14+r11]; ...
+    jmp [rax]               ; Rewrite: mov r11, [gs:eax]; and r11d, -32; add r11, r14; jmp r11
+    call [rbx]              ; Rewrite: mov r11, [gs:ebx]; and r11d, -32; add r11, r14; call r11
+    jmp [fs:rax]            ; Rewrite: mov r11, [r15+32]; mov r11, [gs:r11d+eax]; and r11d, -32; ...
     jmp label               ; Safe: direct jump -> no rewrite
 
     ;; --- Returns ---
